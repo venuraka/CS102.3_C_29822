92018a8b52e8bc94003990117cae61cf62746c8b
venuraka ranasinghe <132426672+venuraka@users.noreply.github.com>
2023-07-26 22:32:14 +0530

    Add files via upload

---
+++ practical 1/04.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int number1,number2,total;
+    printf("Input two numbers");
+    scanf("%d %d",&number1,&number2);
+    total=number1+number2;
+    printf("Total is %d\n",total);
+
+
+}
