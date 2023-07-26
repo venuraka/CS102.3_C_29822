ce18ca21184be405f9e74c09266e8fbb550a3c45
venuraka ranasinghe <132426672+venuraka@users.noreply.github.com>
2023-07-26 22:47:26 +0530

    Add files via upload

---
+++ practical 1/07.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int number1,number2;
+    printf("Input two numbers");
+    scanf("%d %d",&number1,&number2);
+    printf("the number before swap is %d%d\n",number1,number2);
+    printf("the number afte swap is %d%d\n",number2,number1);
+
+}
