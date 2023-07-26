0be21a5283dee0fabad6a5f2bd28f5ef38ae96b6
venuraka ranasinghe <132426672+venuraka@users.noreply.github.com>
2023-07-26 22:37:20 +0530

    Add files via upload

---
+++ practical 1/05.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float number1,number2,average;
+    printf("Input two decimal numbers");
+    scanf("%f %f",&number1,&number2);
+    average=(number1+number2)/2;
+    printf("average is %f \n",average);
+
+}
