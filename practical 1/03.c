bd8a9f837505cea5c18cd0f60307e22fecded8e1
venuraka ranasinghe <132426672+venuraka@users.noreply.github.com>
2023-07-26 22:21:56 +0530

    Add files via upload

---
+++ practical 1/03.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int integer;
+    float decimalfloat;
+    double decimaldouble;
+    char character;
+    printf("Enter a nuber\n");
+    scanf("%d",&integer);
+    printf("Enter a decimal for float\n");
+    scanf("%f",&decimalfloat);
+    printf("Enter a decimal for double\n");
+    scanf("%lf",&decimaldouble);
+    printf("enter a character\n");
+    scanf("%lf",&decimaldouble);
+    scanf("%c",&character);
+    printf("Number is %d\n",integer);
+    printf("Decimal for float is %f\n",decimalfloat);
+    printf("Decimal for double is %f\n",decimaldouble);
+    printf("Character is %c\n",character);
+
+
+
+
+}
